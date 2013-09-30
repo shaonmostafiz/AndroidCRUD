51e8ced38315f9bb8191fce3090c7681197fde81
shaonmostafiz <shaon.iit08@gmail.com>
2013-09-30 22:09:25 +0600

    first commit

---
+++ src/com/example/androidcrud/AndroidSQLiteTutorialActivity.java
@@ -0,0 +1,41 @@
+package com.example.androidcrud;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class AndroidSQLiteTutorialActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_android_sqlite_tutorial);
+
+		DatabaseHandler db = new DatabaseHandler(this);
+		
+
+		/**
+		 * CRUD Operations
+		 * */
+		// Inserting Contacts
+		Log.d("Insert: ", "Inserting ..");
+		db.addContact(new Contact("Ravi", "9100000000"));
+		db.addContact(new Contact("Srinivas", "9199999999"));
+		db.addContact(new Contact("Tommy", "9522222222"));
+		db.addContact(new Contact("Karthik", "9533333333"));
+		db.deleteContact(new Contact("Ravi", "9100000000"));
+
+		// Reading all contacts
+		Log.d("Reading: ", "Reading all contacts..");
+		List<Contact> contacts = db.getAllContacts();
+
+		for (Contact cn : contacts) {
+			String log = "Id: " + cn.getID() + " ,Name: " + cn.getName()
+					+ " ,Phone: " + cn.getPhoneNumber();
+			// Writing Contacts to log
+			Log.d("Name: ", log);
+		}
+	}
+}
